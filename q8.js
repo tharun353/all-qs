e77c6f9bcb55c84a4e526dd764772a10c6be2c58
thatun353 tharunnarasimman76@gmail.com <tharunnarasimman76@gmail.com>
2025-01-19 11:50:13 +0530

    Created q8.js

---
+++ q8.js
@@ -0,0 +1,11 @@
+let mark = 92
+if(mark>=90)
+    console.log("Your grade is A")
+else if(mark>=80)
+    console.log("Your grade is B")
+else if(mark>=70)
+    console.log("Your grade is C")
+else if(mark>=60)
+    console.log("Your grade is D")
+else
+    console.log("Your grade is F    ")
